d3e5f0bec9849ec87dcc29af805ed407a969f98d
Mateusz Wu <mw@softhogs.com>
2015-11-17 15:48:12 +0100

    update test.js

--- test.js
+++ test.js
@@ -1,12 +1,12 @@
 
 var sms = require('./'),
     sender = sms.setSender({
-      api_key: '3663b318be47d10650c9108f7a7d870c',
-      api_secret: 'be7b99f65b8121109ccce0d7591ea5fd'
+      api_key: '',
+      api_secret: ''
     }),
 
     msg = new sms.Message();
-    msg.to('790793138').message('Hello world!');
+    msg.to('').message('Hello world!');
     sender.send(msg, function(err, response){
         console.log(err, response)
     });
